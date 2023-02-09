8179748c6f0382ebbe042e8e51efe5d96040103b
taha7900 <taha7900@gmail.com>
2023-02-09 18:42:10 +0330

    Test Route and View added working just fine

---
+++ backend/apps/authentication/views/TestView.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestView(APIView):
+    http_method_names = ['get']
+
+    def get(self, request, pk):
+        return Response(data={
+            "message": "Route Is Working Just Fine!"
+        })
