bfef96de1788e0e64e6023644d1fd1a9b678cf01
rerfan24 <rezaerfan26@gmail.com>
2023-02-12 02:47:00 +0330

    Add HomePage

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarItemComponent } from './components/navbar-item/navbar-item.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarItemComponent,
+    LoginComponent,
+    HomepageComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent}
+    ]),
+
+    // NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/my-app/src/app/components/navbar-item/navbar-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-item',
+  templateUrl: './navbar-item.component.html',
+  styleUrls: ['./navbar-item.component.css']
+})
+export class NavbarItemComponent {
+  @Input() text!: string;
+  @Input() link!: string;
+
+  @Output() itemClicked = new EventEmitter();
+
+  constructor() { }
+
+  onClick() {
+    this.itemClicked.emit();
+  }
+}
