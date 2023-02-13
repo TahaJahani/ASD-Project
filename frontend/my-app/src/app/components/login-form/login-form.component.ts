bd3f590bd2405e2f9fcc5bb1a057dc2f7486976f
rerfan24 <rezaerfan26@gmail.com>
2023-02-14 02:42:52 +0330

    Add Login form preliminary design

--- frontend/my-app/src/app/components/login-form/login-form.component.ts
+++ frontend/my-app/src/app/components/login-form/login-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  @Output() login : EventEmitter<any> = new EventEmitter();
+
+  username: string = '';
+  password: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.username === '' || this.password === '') {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    alert('received username: ' + this.username + ' and password: ' + this.password + '');
+    // this.login.emit({username: this.username, password: this.password});
+  }
 
 }
--- frontend/my-app/src/app/services/user-service.service.ts
+++ frontend/my-app/src/app/services/user-service.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../Interfaces/User';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-  private apiURL = 'http://localhost:3000/auth';
+  private apiURL = 'http://37.32.13.83/auth';
 
   constructor(private http : HttpClient) { }
 
   signUp(user : User) : Observable<any> {
     const url = `${this.apiURL}/sign-up`;
-    return this.http.post(url, user);
+    return this.http.post(url, user, httpOptions);
   }
 }
