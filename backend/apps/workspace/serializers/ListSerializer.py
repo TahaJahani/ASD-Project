59491939dd2528007a0f9e8d0b792753f53499e0
taha7900 <taha7900@gmail.com>
2023-03-07 23:56:55 +0330

    Fix minor bug

--- backend/apps/workspace/serializers/ListSerializer.py
+++ backend/apps/workspace/serializers/ListSerializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from apps.workspace.models import List
-from apps.workspace.serializers import CardSerializer
+from . import CardSerializer
 
 
 class ListSerializer(serializers.ModelSerializer):
