bf1980b6e1a54991501676d6d0b73a54248cddb0
taha7900 <taha7900@gmail.com>
2023-03-07 23:44:04 +0330

    Fix minor bug

--- backend/apps/workspace/views/ListView.py
+++ backend/apps/workspace/views/ListView.py
@@ -3,19 +3,20 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from apps.workspace.models import *
-from apps.workspace.serializers import ListSerializer
+from apps.workspace.serializers import ListSerializer, ListDetailSerializer
 from apps.workspace.serializers.BoardSerializer import BoardSerializer
 from django.shortcuts import get_object_or_404
 
 
 class ListLists(ListAPIView):
     authentication_classes = [IsAuthenticated]
+    serializer_class = ListDetailSerializer
 
     def get_queryset(self):
         board_id = self.request.GET.get('board_id')
         user = self.request.user
         lists = List.objects.filter(board_id=board_id, board__users__in=[user]).all()
-        return Response(ListSerializer(lists, many=True))
+        return lists
 
 
 class CreateList(APIView):
