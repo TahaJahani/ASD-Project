6901ae24eba3bf4c9b45ad9c038a007210191338
taha7900 <taha7900@gmail.com>
2023-03-07 22:36:38 +0330

    Add card serializer

---
+++ backend/apps/workspace/views/CardView.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from apps.workspace.models import *
+from apps.workspace.serializers.BoardSerializer import BoardSerializer
+from django.shortcuts import get_object_or_404
+
+
+class AddCard(APIView):
+    authentication_classes = [IsAuthenticated]
+
+    def post(self, request):
+        list_id = request.POST.get('list_id')
+        related_list = List.objects.filter(pk=list_id, board__users__in=[request.user])
+        related_list = get_object_or_404(related_list)
+        card_title = request.POST.get('title')
+        card_order = Card.objects.filter(list=related_list).count()
+        card = Card.objects.create(
+            list=related_list,
+            order=card_order,
+            title=card_title
+        )
+        return Response({
+            "Status": "Ok",
+            "Message": "Task created",
+            "Task": card.to_dict()
+        })
