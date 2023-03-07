898be41a1e82bd04776d722c2e1e933683ab8f3e
taha7900 <taha7900@gmail.com>
2023-03-08 01:11:14 +0330

    Add models to site admin

--- backend/apps/workspace/admin.py
+++ backend/apps/workspace/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.workspace.models import *
+
+admin.site.register(Board)
+admin.site.register(List)
+admin.site.register(Card)
+admin.site.register(JoinRequest)
