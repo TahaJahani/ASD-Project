e36f627eccd15a2ca3f7cc7d26cde61e58535bde
taha7900 <taha7900@gmail.com>
2023-03-07 23:55:07 +0330

    Add logout api

--- backend/apps/authentication/views/LoginView.py
+++ backend/apps/authentication/views/LoginView.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -28,3 +29,12 @@ class LoginView(APIView):
             return Response(data={
                 "error": "wrong username or password"
             }, status=403)
+
+
+class LogoutView(APIView):
+    authentication_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response({"message": "Logged out successfully"})
